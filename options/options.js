044b649b6c70b87e76f95aa03222e75015742b1a
Crowdbotics <team@crowdbotics.com>
2023-07-23 20:48:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "smartbudget-42373",
+  url: "https://smartbudget-42373.botics.co",
+  api: "https://smartbudget-42373.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
